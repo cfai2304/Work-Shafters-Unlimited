f550a037975642be2ec9c6b5a57410abfe3ad1e3
Anthony Arrowood <36271412+AnthonyA28@users.noreply.github.com>
2019-10-07 13:01:08 -0400

    Update Cooling_tower.py

--- Cooling_tower.py
+++ Cooling_tower.py
@@ -1,14 +1,15 @@
-h = 0.98 # humidity as defined in the theory
+specific_humidity = 14.4
+h = specific_humidity/1000 # humidity as defined in the theory
 H_g_out = 2 # enthalpy of air leaving
 H_g_in = 3 # enthalpy of air entering
-T_l_out = 4 # temperature of the water out
+T_l_out = 28 # temperature of the water out
 H_0 = 234 # reference enthalpy
 T_0 = 234 # reference temp
-latent_heat = 324 #latent heat of evapo of water
+latent_heat = 2401 #latent heat of evapo of water
 N = 100
-G = 34 # gas flow rate
-C_l = 41.8 # heat capacity of liquid water
-L = 231 # water flow rate
+G = 0.42 # gas flow rate
+C_l = 4.18 # heat capacity of liquid water
+L = 0.004877 # water flow rate
 
 
 def dH_g (N): # eq 2.23
@@ -27,7 +28,6 @@ def f_H_g(H_star, _H_g):
 	return (1/(H_star - _H_g))
 
 
-
 H_g = [None]*N
 T_l = [None]*N
 H_star = [None]*N
@@ -37,9 +37,25 @@ T_l[0] = T_l_out
 H_star[0] = _H_star(T_l[0])
 
 
+
+# create the table
 for n in range (1,N): # [1,99]
 	H_g[n] = H_g[n-1] + dH_g(N)
 	T_l[n] = _T_l(H_g[n])
 	H_star[n] = _H_star(T_l[n])
 	print(H_g[n], "," , T_l[n],",", H_star[n])
+
+# apply the trapezoidal rule to determine the integral eq2.20
+sum = 0
+for k in range(1, N-1):
+	fun_H_g_k = (1/(H_star[k] - H_g[k]))
+	fun_H_g_k_plus1 = (1/(H_star[k+1] - H_g[k+1]))
+	trap = ( fun_H_g_k + fun_H_g_k_plus1 )/2.0 * dH_g(N)
+	sum += trap
+
+
+print("Sum = n_OG: " , sum)
+
+
+
 
